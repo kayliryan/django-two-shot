1b2aa82bb55140a92581a8e78b7e0ef088281513
Kayli <kayliryan@gmail.com>
2022-06-15 14:58:05 -0700

    Feature 11 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
-from receipts.views import ReceiptListView
+from receipts.views import (
+    ReceiptListView,
+    ReceiptCreateView,
+)
 
-urlpatterns = [path("", ReceiptListView.as_view(), name="home")]
+
+urlpatterns = [
+    path("", ReceiptListView.as_view(), name="home"),
+    path("create/", ReceiptCreateView.as_view(), name="create_receipt"),
+]
--- receipts/views.py
+++ receipts/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from receipts.models import Receipt
@@ -16,3 +18,22 @@ class ReceiptListView(LoginRequiredMixin, ListView):
         # makes sense that it's purchaser because that's defined in our model, not
         # owner or user. .... the .user comes from the USER_MODEL so it's defined
         # by django in the black magic, not by us
+
+
+class ReceiptCreateView(LoginRequiredMixin, CreateView):
+    model = Receipt
+    template_name = "receipts/create.html"
+    fields = [
+        "vendor",
+        "total",
+        "tax",
+        "date",
+        "category",
+        "account",
+    ]
+
+    def form_valid(self, form):
+        receipt = form.save(commit=False)
+        receipt.purchaser = self.request.user
+        receipt.save()
+        return redirect("home")
