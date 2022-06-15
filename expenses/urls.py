2bb24e51ce558e819c63031cbb9238c7aab1ed09
Kayli <kayliryan@gmail.com>
2022-06-15 11:33:23 -0700

    Feature 6 Complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("home")), name="home"),
     path("receipts/", include("receipts.urls")),
 ]
